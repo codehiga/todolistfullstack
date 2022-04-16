4126b717052aaf5b680ac8a11713efa5aab5c9bc
codehiga <103391897+codehiga@users.noreply.github.com>
2022-04-16 07:06:21 -0300

    Delete client directory

--- client/src/hooks/useQueryContext.js
+++
@@ -1,6 +0,0 @@
-import React, {useContext} from "react"
-import { QueriesContext } from "../contexts/queriesContext"
-
-export const useQueryContext = () => {
-  return useContext(QueriesContext);
-}
